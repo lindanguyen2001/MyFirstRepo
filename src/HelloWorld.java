e3e0a4802320cfb1e8be236630bac7f88d213e25
Linda Nguyen <nguyenc8@miamioh.edu>
2020-05-04 03:09:42 +0700

    My first commit in Git

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,19 +1,5 @@
-import java.util.LinkedList;
-import java.util.ListIterator;
-import java.util.Stack;
-
 public class HelloWorld {
-
 	public static void main(String[] args) {
-		Stack<String> stringStack = new Stack<>();
-		stringStack.push("ab");
-		stringStack.push("abc");
-		stringStack.push("a");
-		while (!stringStack.empty())
-		{ 
-		 System.out.print(stringStack.pop() + ",");
-		}
-		 
-		
+		System.out.println("Hello World!");
 	}
 }
